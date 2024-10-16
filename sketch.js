1797788036db00af31c14d968593b4f7779fcc26
anna julia <165907503+annalara03@users.noreply.github.com>
2024-10-16 08:01:43 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+  background("white");
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+ 
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
